b7c7a6149b18a9327fe666cfdc7ccc6480a6e3b8
raydedon <raydedon@gmail.com>
2019-06-16 22:56:54 +0530

    changed to scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import Home from './component/home/Home';
